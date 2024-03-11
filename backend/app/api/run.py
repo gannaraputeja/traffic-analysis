79f664a0ca348b2dcf4e7033cf352d3c018dac6b
raviteja <gannaraputeja@gmail.com>
2024-03-11 03:53:49 -0700

    Adding start/run api to process video in background

--- backend/app/api/run.py
+++ backend/app/api/run.py
@@ -1,14 +1,23 @@
 from typing import Any
 
+from fastapi import BackgroundTasks
 from fastapi.routing import APIRouter
 from starlette.responses import Response
 
 from app.schemas.msg import Msg
-from app.deps.db import CurrentAsyncSession
+from app.yolo.ultralytics import VideoProcessor
 
 router = APIRouter()
 
 
 @router.get("/start/run", response_model=Msg)
-def start_run() -> Any:
-    return {"msg": "Hello world!"}
+async def start_run(background_tasks: BackgroundTasks) -> Any:
+    processor = VideoProcessor(
+        source_weights_path='data/traffic_analysis.pt',
+        source_video_path='data/traffic_analysis.mov',
+        target_video_path='data/traffic_analysis_result.mov',
+        confidence_threshold=0.3,
+        iou_threshold=0.5,
+    )
+    background_tasks.add_task(processor.process_video)
+    return {"msg": "Started processing video in the background."}
